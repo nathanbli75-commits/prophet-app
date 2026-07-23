7465ec3a06df9a4c71cabdee7cb568caa771b8a8
nathanbli75-commits <nathanbli75@gmail.com>
2026-07-23 14:57:20 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // sans jamais mélanger les versions, et sans que l'utilisateur ait à
 // désinstaller/réinstaller quoi que ce soit.
 
-const CACHE_VERSION = 'guelane-v3.32';   // ← CHANGE ce numéro à chaque déploiement
+const CACHE_VERSION = 'guelane-v3.33';   // ← CHANGE ce numéro à chaque déploiement
 const CACHE_NAME = CACHE_VERSION;
 
 // À l'installation d'une nouvelle version : on l'active tout de suite
